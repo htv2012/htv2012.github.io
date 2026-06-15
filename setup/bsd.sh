889b285c80b1ba9c7c91200907049ae0a6184d0c
Hai Vu <haivu2004@gmail.com>
2026-06-15 07:15:58 -0700

    feat: update BSD installation

---
+++ setup/bsd.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+userId=$(id -u)
+if [ "$userId" -ne 0 ]; then
+	echo Must be root to run this script
+    exit
+fi
+
+echo Installing tools
+pkg update && pkg upgrade -y
+pkg install -y bash
+pkg install -y bat
+pkg install -y eza
+pkg install -y fzf
+pkg install -y git
+pkg install -y git-delta
+pkg install -y python
+pkg install -y ripgrep
+pkg install -y stow
+pkg install -y syncthing
+pkg install -y tmux
+pkg install -y tree
+pkg install -y vim
+pkg install -y zsh
+
